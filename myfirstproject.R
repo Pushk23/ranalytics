802aef0d7dbe14b50d986d111b36453fb05db089
Pushk23 <115263558+Pushk23@users.noreply.github.com>
2022-10-10 23:37:15 +0530

    my first project

---
+++ myfirstproject.R
@@ -0,0 +1,35 @@
+mtcars
+#control + enter
+x1 = c(12,32,45)
+x1 [2]
+(x2 = 1:100) #assingh and print
+print (x2 [15])
+print (x2 [25])
+class (x2) # show class of values
+class (x1)
+mean (x2)
+x3 = 1:25
+x3  #no need to write print it will understand
+sd (x3)
+sd (x2)
+ls () # variables in environment
+data() # shows data set avaliable in environment
+women # prints women dataset
+?AirPassengers # ? shows information about datasets
+library() 
+?mean #help
+help(mean)
+??mean #search through other sources
+x = c(1:10,50,100,200)
+xm = mean(x)
+mean (x)
+mean(x, trim= 0.10)
+?c
+version #version of R
+Sys.Date() #date of system
+c (mean(x), mean(x, trim = 0.10))
+getwd() #working directory
+?methods
+methods(class = 'matrix')
+plot(10:100) #basic command to plot
+plot(women)
